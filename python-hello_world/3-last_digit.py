4de69ebc57a2d9f034544dca2cfc53734a0eb055
zlater007 <samsonofubu@yahoo.com>
2024-02-10 16:05:43 +0100

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,15 +1,16 @@
+#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-
-print(number)
-
-last_digit = abs(number) % 10
-print("Last digit of the number:", last_digit)
-
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("änd is 0")
+last_number = abs(number) % 10
+if number > 0:
+    last_digit= number % 10
 else:
-    print("and is less than 6 and not 0")
-
+    last_digit= -(-number % 10)
+if last_digit > 5:
+    print("Last digit of",number,"is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+elif last_digit < 6 and last_digit!=0:
+    print( "Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+else:
+    TypeError
